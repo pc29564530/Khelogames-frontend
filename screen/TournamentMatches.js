7d0a972959682b51b49fc26d6423fe1e32e39ee9
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-03-31 11:17:57 +0530

    style the tournament match

--- screen/TournamentMatches.js
+++ screen/TournamentMatches.js
@@ -1,13 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {View, Text, Pressable, Modal} from 'react-native';
+import { View, Text, Pressable, Modal, Image } from 'react-native';
 import { BASE_URL } from '../constants/ApiConstants';
 import useAxiosInterceptor from './axios_config';
 import tailwind from 'twrnc';
 import CreateFixtue from '../components/CreateFixture';
 import { useFocusEffect } from '@react-navigation/native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const TournamentMatches = ({route}) => {
+const TournamentMatches = ({ route }) => {
     const tournament = route.params.tournament;
     const [teams, setTeams] = useState([]);
     const [admin, setAdmin] = useState(false);
@@ -46,11 +48,10 @@ const TournamentMatches = ({route}) => {
                 }
             });
             const item = response.data;
-
             item.forEach(item => {
                 if (item.organizer_name.toLowerCase() === user.toLowerCase()) {
                     setAdmin(true);
-                    setOrganizerID(item.organizer_id)
+                    setOrganizerID(item.organizer_id);
                 }
             });
         } catch (err) {
@@ -86,8 +87,22 @@ const TournamentMatches = ({route}) => {
                     'Content-Type': 'application/json'
                 }
             });
-            const item = response.data;
-            const allMatchData = await Promise.all(item.map(async (item) => {
+            const item = response.data.map((item) => {
+                //date
+                const timestampStrDate = item.date_on;
+                const timestampDate = new Date(timestampStrDate);
+                const optionsDate = { weekday: 'long', month: 'long', day: '2-digit' };
+                const formattedDate = timestampDate.toLocaleString('en-US', optionsDate);
+                //time
+                const timestampStr = item.start_at;
+                const timestamp = new Date(timestampStr);
+                const optionsTime = { hour: '2-digit', minute: '2-digit', hour12: true };
+                const formattedTime = timestamp.toLocaleTimeString('en-US', optionsTime);
+                item.date_on = formattedDate;
+                item.start_at = formattedTime;
+                return item;
+            });
+            const matchData = item.map(async (item) => {
                 try {
                     const authToken = await AsyncStorage.getItem('AccessToken');
                     const response1 = await axiosInstance.get(`${BASE_URL}/getClub/${item.team1_id}`, null, {
@@ -102,11 +117,18 @@ const TournamentMatches = ({route}) => {
                             'Content-Type': 'application/json'
                         }
                     });
-                    return {...item, team1_name: response1.data.club_name, team2_name: response2.data.club_name};
+                    const response3 = await axiosInstance.get(`${BASE_URL}/getTournament/${item.tournament_id}`, null, {
+                        headers: {
+                            'Authorization': `bearer ${authToken}`,
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    return { ...item, team1_name: response1.data.club_name, team2_name: response2.data.club_name, tournament_name: response3.data.tournament_name };
                 } catch (err) {
                     console.error("Unable to fetch club data: ", err);
                 }
-            }));
+            });
+            const allMatchData = await Promise.all(matchData);
             setTournamentTeamData(allMatchData);
         } catch (err) {
             console.error("Unable to fetch tournament matches: ", err);
@@ -114,39 +136,59 @@ const TournamentMatches = ({route}) => {
     };
 
     return (
-        <View style={tailwind`flex-1 mt-1 items-center justify-center`}>
-           {admin && (
-                <Pressable onPress={() => setIsModalVisible(!isModalVisible)} style={tailwind`rounded-lg bg-purple-200 mt-10 p-2`}>
-                    <Text style={tailwind`text-lg`}>Set Fixture</Text>
-                </Pressable>
-           )}
-           
-           {isModalVisible && (
+        <ScrollView 
+            contentContainerStyle={{flexGrow:1}}
+            nestedScrollEnabled={true}
+        >
+        <View style={tailwind`flex-1 bg-gray-100`}>
+            <View style={tailwind`p-4 flex-row justify-between items-center bg-white`}>
+                <Text style={tailwind`text-xl font-bold text-gray-800`}>{tournament.name}</Text>
+                {admin && (
+                    <Pressable onPress={() => setIsModalVisible(!isModalVisible)} style={tailwind`rounded-lg bg-purple-200 p-2 justify-start flex-row items-center`}>
+                        <Text style={tailwind`text-lg text-purple-800`}>Set Fixture</Text>
+                        <MaterialIcons name="add" size={24} color="black" />
+                    </Pressable>
+                )}
+            </View>
+            {isModalVisible && (
                 <Modal transparent={true} animationType='slide' visible={isModalVisible}>
                     <View style={tailwind`flex-1 justify-center bg-black bg-opacity-50`}>
-                        <CreateFixtue 
+                        <CreateFixtue
                             tournament={tournament}
                             teams={teams}
                             organizerID={organizerID}
                         />
                     </View>
                 </Modal>
-           )}
-            <View>
-            {tournamentTeamData?.length > 0 ? (
-                tournamentTeamData.map((item, index) => (
-                    <Pressable key={index} style={tailwind`mt-4 rounded-lg bg-purple-300 p-4 flex-row items-center justify-evenly`}>
-                        <Text style={tailwind`text-xl`}>{item.team1_name}</Text>
-                        <Text style={tailwind`text-xl`}>vs</Text>
-                        <Text style={tailwind`text-xl`}>{item.team2_name}</Text>
-                    </Pressable>
-                ))
-            ) : (
-                <Text>Loading matches...</Text>
             )}
+            <View style={tailwind`p-4`}>
+                {tournamentTeamData?.length > 0 ? (
+                    tournamentTeamData.map((item, index) => (
+                        <Pressable key={index} style={tailwind`mb-4 p-4 bg-white rounded-lg shadow-md`}>
+                            <View style={tailwind`flex-row justify-between items-center mb-2 gap-2 p-2`}>
+                                <View style={tailwind`items-center`}>
+                                    <Image source={{ uri: item.team1_avatar_url }} style={tailwind`w-10 h-10 bg-violet-200 rounded-full `} />
+                                    <Text style={tailwind`ml-2 text-xl font-semibold text-gray-800`}>{item.team1_name}</Text>
+                                </View>
+                                <Text style={tailwind`text-gray-600 text-lg`}>vs</Text>
+                                <View style={tailwind` items-center`}>
+                                    <Image source={{ uri: item.team2_avatar_url }} style={tailwind`w-10 h-10 bg-violet-200 rounded-full`} />
+                                    <Text style={tailwind`ml-2 text-xl font-semibold text-gray-800`}>{item.team2_name}</Text>
+                                </View>
+                            </View>
+                            <View style={tailwind`flex-row justify-between`}>
+                                <Text style={tailwind`text-gray-600`}>{item.date_on}</Text>
+                                <Text style={tailwind`text-gray-600`}>{item.start_at}</Text>
+                            </View>
+                        </Pressable>
+                    ))
+                ) : (
+                    <Text style={tailwind`text-center mt-4 text-gray-600`}>Loading matches...</Text>
+                )}
             </View>
         </View>
+        </ScrollView>
     );
-}
+};
 
 export default TournamentMatches;
