0a11b585c03ed7cc046a2a9c0a9b7c09b8652002
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-04-20 07:28:57 +0530

    add the functionality for the date and time picker

--- components/CreateFixture.js
+++ components/CreateFixture.js
@@ -4,7 +4,7 @@ import {View, Text, Pressable, Modal} from 'react-native';
 import { BASE_URL } from '../constants/ApiConstants';
 import useAxiosInterceptor from '../screen/axios_config';
 import tailwind from 'twrnc';
-import DateTimePicker from '@react-native-community/datetimepicker'
+import DateTimePicker from 'react-native-modern-datepicker'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const CreateFixtue = ({tournament, teams, organizerID, handleCloseFixtureModal}) => {
@@ -12,137 +12,198 @@ const CreateFixtue = ({tournament, teams, organizerID, handleCloseFixtureModal})
     const [team1, setTeam1] = useState(null);
     const [team2, setTeam2] = useState(null);
     const [date,setDate] = useState(new Date());
-    const [time, setTime] = useState(new Date());
+    const [startTime, setStartTime] = useState(new Date());
+    const [endTime, setEndTime] = useState(new Date());
     const [isModalTeamVisible, setIsModalTeamVisible] = useState(false);
     const [showDatePicker, setShowDatePicker] = useState(false);
-    const [showTimePicker, setShowTimePicker] = useState(false);
+    const [showStartTimePicker, setShowStartTimePicker] = useState(false);
+    const [isModalEndTimeVisible, setIsModalEndTimeVisible] = useState(false);
+    const [isModalStartTimeVisible, setIsModalStartTimeVisible] = useState(false);
+    const [isModalDateVisible, setIsModalDateVisible] = useState(false);
     const [modalType, setModalType] = useState('')
     const axiosInstance = useAxiosInterceptor()
 
-    const showDate = () => {
-        setShowDatePicker(true);
+    const modifyDateTime = (newDateTime) => {
+        const [datePart, timePart] = newDateTime.split(' ');
+        const [year, month, day] = datePart.split('/').map(Number);
+        const [hour, minute] = timePart.split(':').map(Number);
+        const matchDateTime = new Date(Date.UTC(year, month - 1, day, hour, minute));
+        return matchDateTime;
     }
 
-    const showTime = () => {
-        setShowTimePicker(true);
+    const handleSelectTeam = (item) => {
+    if(modalType === 'team1') {
+        setTeam1(item.id);
+        
+    } else {
+        setTeam2(item.id);
+    }
+    setIsModalTeamVisible(!isModalTeamVisible)
     }
 
-    const handleDateChange = (event, selectedDate) => {
-
-        const currentDate = selectedDate || date;
-        setDate(currentDate)
-        setShowDatePicker(false)
-    }
-
-    const handleTimeChange = (event, selectedTime) => {
-        if (selectedTime !== undefined) {
-          const hours = selectedTime.getHours();
-          const minutes = selectedTime.getMinutes();
-          const newDate = new Date(date);
-          newDate.setHours(hours);
-          newDate.setMinutes(minutes);
-          const timeZoneOffSet = newDate.getTimezoneOffset();
-          newDate.setMinutes(newDate.getMinutes()-timeZoneOffSet);
-          setTime(newDate);
-          setShowTimePicker(false)
-          setShowDatePicker(false)
-        }
-      };
-
-      const handleSelectTeam = (item) => {
-        if(modalType === 'team1') {
-            setTeam1(item.id);
-            
-        } else {
-            setTeam2(item.id);
-        }
-        setIsModalTeamVisible(!isModalTeamVisible)
-      }
-
-      const handleSetFixture = async () => {
-            try {
-                const fixture = {
-                    organizer_id:organizerID,
-                    tournament_id: tournament.tournament_id,
-                    team1_id: team1,
-                    team2_id: team2,
-                    date_on: date,
-                    start_at: time,
-                    stage:'',
-                    sports:tournament.sport_type
-                }
-                const authToken = await AsyncStorage.getItem('AccessToken');
-                const response = await axiosInstance.post(`${BASE_URL}/createTournamentMatch`, fixture,{
-                    headers: {
-                        'Authorization':`bearer ${authToken}`,
-                        'Content-Type':'application/json'
-                    }
-                })
-                handleCloseFixtureModal()
-            } catch (err) {
-                console.error("unable to set the fixture : ", err);
+    const handleSetFixture = async () => {
+        
+        try {
+            const fixture = {
+                organizer_id:organizerID,
+                tournament_id: tournament.tournament_id,
+                team1_id: team1,
+                team2_id: team2,
+                date_on: modifyDateTime(date),
+                start_time: modifyDateTime(startTime),
+                stage:"",
+                sports:tournament.sport_type,
+                end_time: modifyDateTime(endTime)
             }
-      }
 
-      return (
-        <View style={tailwind`rounded-lg bg-white p-10  m-8`}>
-            <Pressable onPress={showDate} style={tailwind`mb-2`}>
-                <MaterialIcons name="date-range" size={24} color="black" />
+            const authToken = await AsyncStorage.getItem('AccessToken');
+            const response = await axiosInstance.post(`${BASE_URL}/createTournamentMatch`, fixture,{
+                headers: {
+                    'Authorization':`bearer ${authToken}`,
+                    'Content-Type':'application/json'
+                }
+            })
+
+            const item = response.data;
+            if(item && item !== null) {
+                try {
+                    const scoreData1 = {
+                        match_id:item.match_id,
+                        tournament_id: item.tournament_id,
+                        team_id: item.team1_id,
+                        goal_score: 0
+                    }
+                    const scoreData2 = {
+                        match_id:item.match_id,
+                        tournament_id: item.tournament_id,
+                        team_id: item.team2_id,
+                        goal_score: 0
+                    }
+                    const team1Response = await axiosInstance.post(`${BASE_URL}/addFootballMatchScore`,scoreData1, {
+                        headers: {
+                            'Authorization':`bearer ${authToken}`,
+                            'Content-Type':'application/json'
+                        }
+                    });
+                    const team2Response = await axiosInstance.post(`${BASE_URL}/addFootballMatchScore`,scoreData2, {
+                        headers: {
+                            'Authorization':`bearer ${authToken}`,
+                            'Content-Type':'application/json'
+                        }
+                    });
+                } catch ( err) {
+                    console.error("unable to create the score board ", err)
+                }
+            }
+            handleCloseFixtureModal()
+        } catch (err) {
+            console.error("unable to set the fixture : ", err);
+        }
+    }
+
+    const hideDatePicker = () => {
+    setShowDatePicker(false);
+    };
+      
+    return (
+    <View style={tailwind`rounded-lg bg-white p-10  m-8`}>
+        <Pressable onPress={() => setIsModalDateVisible(!isModalDateVisible)} style={tailwind`mb-2`}>
+            <MaterialIcons name="date-range" size={24} color="black" />
+        </Pressable>
+        <View style={tailwind`flex-row mb-2 gap-2`}>
+            <Pressable onPress={() => setIsModalStartTimeVisible(!isModalStartTimeVisible)} style={tailwind`rounded-lg bg-red-300 p-2`} >
+                <Text> Start Time</Text>
             </Pressable>
-            <Pressable onPress={showTime} style={tailwind`mb-2`}>
-                <MaterialIcons name="access-time" size={24} color="black" />
+            <Pressable onPress={() => setIsModalEndTimeVisible(!isModalEndTimeVisible)} style={tailwind`rounded-lg bg-red-300 p-2`} >
+                <Text>End Time</Text>
             </Pressable>
-            <View style={tailwind`flex-row justify-between mb-2`}>
-            <Pressable onPress={() => {setModalType('team1'), setIsModalTeamVisible(!isModalTeamVisible)}} style={tailwind`rounded-lg bg-red-300 p-2`}>
-                    <Text style={tailwind`text-center`}>Team 1</Text>
-            </Pressable>
-            <Text style={tailwind`text-center`}>Vs</Text>
-            <Pressable onPress={() =>{setModalType('team2'), setIsModalTeamVisible(!isModalTeamVisible)}} style={tailwind`rounded-lg bg-red-300 p-2`}>
-                    <Text style={tailwind`text-center`}>Team 2</Text>
-            </Pressable>
-            </View>
-            <Pressable onPress={()=>handleSetFixture()} style={tailwind`rounded-lg bg-red-300 p-2 w-20 items-center`}>
-                <Text>Submit</Text>
-            </Pressable>
-            {showDatePicker && (
-                <DateTimePicker 
-                    testID='dateTimePicker'
-                    value={date}
-                    mode='date'
-                    is24Hour={true}
-                    display='default'
-                    onChange={handleDateChange}
-                />
-            )}
-            {showTimePicker && (
-                <DateTimePicker 
-                    testID="timePicker"
-                    value={time}
-                    mode="time"
-                    is24Hour={true}
-                    display='default'
-                    onChange={handleTimeChange}
-                />
-            )}
-            {isModalTeamVisible && (
-                <Modal
-                    transparent={true}
-                    animationType="slide"
-                    visible={isModalTeamVisible}
-                >
-                    <View style={tailwind`flex-1 justify-end bg-black bg-opacity-50`}>
-                        <View style={tailwind`bg-white rounded-md p-4`}>
-                            {teams.map((item,index) => (
-                                <Pressable key={index} onPress={() => handleSelectTeam(item)} style={tailwind`mt-2 p-1`}>
-                                    <Text style={tailwind`text-black text-lg`}>{item.club_name}</Text>
-                                </Pressable>
-                            ))}
-                        </View>
-                    </View>
-                </Modal>
-               )}
         </View>
-      );
+        <View style={tailwind`flex-row justify-between mb-2`}>
+        <Pressable onPress={() => {setModalType('team1'), setIsModalTeamVisible(!isModalTeamVisible)}} style={tailwind`rounded-lg bg-red-300 p-2`}>
+                <Text style={tailwind`text-center`}>Team 1</Text>
+        </Pressable>
+        <Text style={tailwind`text-center`}>Vs</Text>
+        <Pressable onPress={() =>{setModalType('team2'), setIsModalTeamVisible(!isModalTeamVisible)}} style={tailwind`rounded-lg bg-red-300 p-2`}>
+                <Text style={tailwind`text-center`}>Team 2</Text>
+        </Pressable>
+        </View>
+        <Pressable onPress={()=>handleSetFixture()} style={tailwind`rounded-lg bg-red-300 p-2 w-20 items-center`}>
+            <Text>Submit</Text>
+        </Pressable>
+        {isModalDateVisible && (
+            <Modal
+            transparent={true}
+            animationType="slide"
+            visible={isModalDateVisible}
+        >
+            <View style={tailwind`flex-1 justify-end bg-black bg-opacity-50`}>
+            <View style={tailwind`bg-white rounded-md p-4`}>
+                <DateTimePicker
+                    onSelectedChange={(date) => {
+                        setDate(date);
+                        setIsModalDateVisible(false);
+                    }}
+
+                />
+            </View>
+            </View>
+        </Modal>
+        )}
+        {isModalEndTimeVisible && (
+            <Modal
+            transparent={true}
+            animationType="slide"
+            visible={isModalEndTimeVisible}
+        >
+            <View style={tailwind`flex-1 justify-end bg-black bg-opacity-50`}>
+            <View style={tailwind`bg-white rounded-md p-4`}>
+                <DateTimePicker
+                    onSelectedChange={(endTime) => {
+                        setEndTime(endTime);
+                        setIsModalEndTimeVisible(false);
+                    }}
+                />
+            </View>
+            </View>
+        </Modal>
+        )}
+        {isModalStartTimeVisible && (
+            <Modal
+            transparent={true}
+            animationType="slide"
+            visible={isModalStartTimeVisible}
+        >
+            <View style={tailwind`flex-1 justify-end bg-black bg-opacity-50`}>
+            <View style={tailwind`bg-white rounded-md p-4`}>
+                <DateTimePicker
+                    onSelectedChange={(startTime) => {
+                        setStartTime(startTime);
+                        setIsModalStartTimeVisible(false);
+                    }}
+                />
+            </View>
+            </View>
+        </Modal>
+        )}
+        {isModalTeamVisible && (
+            <Modal
+                transparent={true}
+                animationType="slide"
+                visible={isModalTeamVisible}
+            >
+                <View style={tailwind`flex-1 justify-end bg-black bg-opacity-50`}>
+                    <View style={tailwind`bg-white rounded-md p-4`}>
+                        {teams.map((item,index) => (
+                            <Pressable key={index} onPress={() => handleSelectTeam(item)} style={tailwind`mt-2 p-1`}>
+                                <Text style={tailwind`text-black text-lg`}>{item.club_name}</Text>
+                            </Pressable>
+                        ))}
+                    </View>
+                </View>
+            </Modal>
+            )}
+    </View>
+    );
 }
 
 export default CreateFixtue;
