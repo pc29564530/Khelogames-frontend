fa50ac7684d9401daa5b7f674b08c8ff65317e33
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-05-20 11:03:37 +0530

    fix the conflict of redux and useContext

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { NativeBaseProvider } from 'native-base';
 import {Provider} from 'react-redux'
 import store from './redux/store';
 import MainNavigation from './navigation/MainNavigation';
+import { GlobalProvider } from './context/GlobalContext';
 
 export default function App() {
-  return (  
-    <NativeBaseProvider>
-      <Provider store={store}>
-        <MainNavigation />
-      </Provider>  
-   </NativeBaseProvider>
+  return ( 
+    <Provider store={store}>
+        <NativeBaseProvider>
+            <GlobalProvider >
+              <MainNavigation />
+            </GlobalProvider>  
+        </NativeBaseProvider>
+    </Provider>
   );
 }
 
