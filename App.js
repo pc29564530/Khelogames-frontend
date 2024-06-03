a2929383c4728602110d8614af14ab7221c1bf99
Pawan Kumar <43681370+pc29564530@users.noreply.github.com>
2024-06-03 12:53:39 +0530

    remove the globalcontext from app.js

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
-import 'react-native-gesture-handler';
 import { NativeBaseProvider } from 'native-base';
 import {Provider} from 'react-redux'
 import store from './redux/store';
 import MainNavigation from './navigation/MainNavigation';
-import { GlobalProvider } from './context/GlobalContext';
 
 export default function App() {
   return ( 
     <Provider store={store}>
         <NativeBaseProvider>
-            <GlobalProvider >
-              <MainNavigation />
-            </GlobalProvider>  
+            <MainNavigation />
         </NativeBaseProvider>
     </Provider>
   );
