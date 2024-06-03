5d3c5582d197ab0f133e9fa54bdc99ef23fa12b0
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-06-03 15:55:05 +0530

    rebase the app remove global context

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NativeBaseProvider } from 'native-base';
 import {Provider} from 'react-redux'
 import store from './redux/store';
 import MainNavigation from './navigation/MainNavigation';
+import { GlobalProvider } from './context/GlobalContext';
 
 export default function App() {
   return ( 
