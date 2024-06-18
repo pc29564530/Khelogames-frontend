4b25399d007b97f5b8dbb3cb1f283b91f1aaf94a
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-06-18 23:48:45 +0530

    added the view, text in file

--- components/ThreadProfileCompement.js
+++ components/ThreadProfileCompement.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, View, Text} from 'react-native';
 import AsyncStorage  from '@react-native-async-storage/async-storage'
 import useAxiosInterceptor from '../screen/axios_config';
 import { useNavigation } from '@react-navigation/native';
