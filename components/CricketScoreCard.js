bb46504d1238b878b3447c2e3f1daf3e9d6ac586
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-08-14 14:31:16 +0530

    wicket fall functionality added

--- components/CricketScoreCard.js
+++ components/CricketScoreCard.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { View, Text, Pressable, FlatList, Modal } from 'react-native';
+import { View, Text, Pressable, FlatList, Modal, TouchableOpacity } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+
 import tailwind from "twrnc";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import useAxiosInterceptor from "../screen/axios_config";
@@ -18,18 +20,50 @@ const convertOversToBalls = (item) => {
     return balls
 }
 
+const wicketsTypes = [
+    {  key:1, label: 'Bowled', value: 'bowled' },
+    { key:2,label: 'Caught', value: 'caught' },
+    { key:3,label: 'Stamp', value: 'stamp' },
+    { key:4,label: 'RunOut', value: 'runOut' },
+    { key:5, label: 'LBW', value: 'lbw' }
+];
+
+const wicketsNumber = [
+    {  key:1, label: 'First', value: 1 },
+    { key:2,label: 'Second', value: 2 },
+    { key:3,label: 'Third', value: 3 },
+    { key:4,label: 'Fourth', value: 4 },
+    { key:5, label: 'Fifth', value: 5 },
+    {  key:6, label: 'Sixth', value: 6 },
+    { key:7,label: 'Seventh', value: 7 },
+    { key:8,label: 'Eighth', value: 8 },
+    { key:9,label: 'Ninth', value: 9 },
+    { key:10, label: 'Tenth', value: 10 }
+];
+
 const CricketScoreCard = ({ route }) => {
     const axiosInstance = useAxiosInterceptor();
     const [updateBatting, setUpdateBatting] = useState(null);
     const [updateBowling, setUpdateBowling] = useState(null);
-
+    const [currentBatsman, setCurrentBatsman] = useState();
+    const [currentBowler, setCurrentBowler] = useState();
+    const [wicketType, setWicketType] = useState("");
+    const [ballNumber, setBallNumber] = useState();
+    const [wicketNumber, setWicketNumber] = useState();
+    const [isWicketModalVisible,setIsWicketModalVisible] = useState(false);
     const [isUpdateBattingVisible, setIsUpdateBattingVisible] = useState(false);
     const [isUpdateBowlingVisible, setIsUpdateBowlingVisible] = useState(false);
+    const [showWicketNumberPicker, setShowWicketNumberPicker] = useState(false);
+    const [showWicketTypePicker, setShowWicketTypePicker] = useState(false);
+    const [showBatsmanPicker, setShowBatsmanPicker] = useState(false);
+    const [showBowlerPicker, setShowBowlerPicker] = useState(false);
+    const [showOversPicker, setShowOverPicker] = useState(false);
     const { matchData, matchID, homeTeamID, awayTeamID } = route.params;
     const [battingData, setBattingData] = useState([]);
     const [bowlingData, setBowlingData] = useState([]);
     const [batTeam, setBatTeam] = useState(homeTeamID);
     const [ballTeam, setBallTeam] = useState(awayTeamID);
+    const [wicketFall, setWicketFall] = useState([]);
 
     useEffect(() => {
         const fetchBatting = async () => {
@@ -61,6 +95,7 @@ const CricketScoreCard = ({ route }) => {
                     match_id: matchID,
                     team_id: batTeam===homeTeamID?awayTeamID:homeTeamID,
                 };
+                console.log("Bowling Data Temp: ", temp)
                 const authToken = await AsyncStorage.getItem('AccessToken');
                 const bowlingScore = await axiosInstance.get(`${BASE_URL}/Cricket/getCricketBowlerFunc`,  {
                     params: temp,
@@ -69,6 +104,7 @@ const CricketScoreCard = ({ route }) => {
                         'Content-Type': 'application/json',
                     },
                 });
+                console.log("Bowling Score: ", bowlingScore.data)
                 setBallTeam(bowlingScore.data.bowlingTeam || {});
                 setBowlingData(bowlingScore.data || []);
             } catch (err) {
@@ -77,6 +113,57 @@ const CricketScoreCard = ({ route }) => {
         };
         fetchBowling();
     }, [batTeam]);
+
+
+    useEffect(() => {
+        const fetchFallOfWickets = async () => {
+            try {
+                const temp = {
+                    match_id: matchID,
+                    team_id: batTeam===homeTeamID?homeTeamID:awayTeamID,
+                };
+                const authToken = await AsyncStorage.getItem('AccessToken');
+                const wicketFallen = await axiosInstance.get(`${BASE_URL}/Cricket/getCricketWickets`,  {
+                    params: temp,
+                    headers: {
+                        'Authorization': `bearer ${authToken}`,
+                        'Content-Type': 'application/json',
+                    },
+                });
+                setWicketFall(wicketFallen.data || []);
+            } catch (err) {
+                console.error("unable to fetch bowling score: ", err);
+            }
+        };
+        fetchFallOfWickets();
+    }, [batTeam]);
+
+    const handleFallOfWickets = async () => {
+        try {
+            const temp = {
+                match_id: matchID,
+                team_id: batTeam===homeTeamID?homeTeamID:awayTeamID,
+                batsman_id: currentBatsman,
+                bowler_id: currentBowler,
+                wickets_number: wicketNumber,
+                wicket_type: wicketType,
+                ball_number: ballNumber
+            };
+            console.log("Temp Data: ", temp)
+            const authToken = await AsyncStorage.getItem('AccessToken');
+            const wicketFallen = await axiosInstance.post(`${BASE_URL}/Cricket/addCricketWicket`, temp, {
+                headers: {
+                    'Authorization': `bearer ${authToken}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+            setIsWicketModalVisible(false);
+            setWicketFall(wicketFallen.data || []);
+            setShowOverPicker(false);
+        } catch (err) {
+            console.error("unable to fetch bowling score: ", err);
+        }
+    }
 
     const handleUpdatePlayerScore = (item) => {
         setUpdateBatting(item);
@@ -233,7 +320,21 @@ const CricketScoreCard = ({ route }) => {
             console.error("unable to update the bowling data: ", err);
         }
     }
-    
+
+    const handleWicketFallen = () => {
+        setIsWicketModalVisible(true);
+    }
+
+    const handleWicketsBall = (item) => {
+        item.ball = item.ball+1;
+        setBallNumber(item.ball);
+        setShowOverPicker(false);
+    }
+
+    console.log("Batsman Data: ", battingData)
+    console.log("Bowing Data: ", bowlingData)
+    console.log("Wicket Fall: ", wicketFall)
+
     return (
         <View style={tailwind`flex-1 p-4`}>
             <View style={tailwind`flex-row justify-evenly items-center mb-4 `}>
@@ -281,6 +382,28 @@ const CricketScoreCard = ({ route }) => {
                         </View>
                     )}
                 />
+            </View>
+
+            {/* Fall of Wickets:  */}
+
+            <View style={tailwind`flex-1 p-4`}>
+                <Text styl={tailwind`text-2xl`}>Fall of Wickets </Text>
+                <Pressable onPress={() => handleWicketFallen()} style={tailwind`items-end border p-2`}>
+                    <Text styl={tailwind`text-xl`}>Add Wickets Fall</Text>
+                </Pressable>
+                <FlatList 
+                    data={wicketFall}
+                    keyExtractor={(item) => item.battingTeam.id.toString()}
+                    ListHeaderComponent={() => (
+                        <View style={tailwind`flex-row justify-between py-2 px-4 bg-gray-100`}>
+                            <Text style={tailwind`text-lg font-bold`}>Wkt No.</Text>
+                            <Text style={tailwind`text-lg font-bold`}>Player</Text>
+                            <Text style={tailwind`text-lg font-bold`}>Score</Text>
+                            <Text style={tailwind`text-lg font-bold`}>Over</Text>
+                        </View>
+                    )}
+                />
+                
             </View>
 
             {isUpdateBattingVisible && (
@@ -406,6 +529,191 @@ const CricketScoreCard = ({ route }) => {
                         </View>
                     </Pressable>
                 </Modal>
+            )}
+
+
+        {/* Wicket Fallen */}
+        {isWicketModalVisible && (
+                <Modal
+                animationType="slide"
+                transparent={true}
+                visible={isWicketModalVisible}
+                onRequestClose={() => setIsWicketModalVisible(false)}
+              >
+                <View style={tailwind`flex-1 justify-end bg-black bg-opacity-50`}>
+                  <View style={tailwind`bg-white rounded-md shadow-md p-4`}>
+                    <Text style={tailwind`text-2xl font-bold mb-4`}>Fall of Wickets</Text>
+
+                    <View style={tailwind`flex flex-row justify-between mb-4`}>
+                      <Text style={tailwind`text-lg`}>Batsman</Text>
+                      <TouchableOpacity onPress={() => setShowBatsmanPicker(true)}>
+                        <MaterialIcon name="add" size={24} />
+                      </TouchableOpacity>
+                    </View>
+
+                    <View style={tailwind`flex flex-row justify-between mb-4`}>
+                      <Text style={tailwind`text-lg`}>Bowler</Text>
+                      <TouchableOpacity onPress={() => setShowBowlerPicker(true)}>
+                      <MaterialIcon name="add" size={24} />
+                      </TouchableOpacity>
+                    </View>
+              
+                    <View style={tailwind`flex flex-row justify-between mb-4`}>
+                      <Text style={tailwind`text-lg`}>Wicket Type</Text>
+                      <TouchableOpacity onPress={() => setShowWicketTypePicker(true)}>
+                        <MaterialIcon name="add" size={24} />
+                      </TouchableOpacity>
+                    </View>
+              
+                    <View style={tailwind`flex flex-row justify-between mb-4`}>
+                      <Text style={tailwind`text-lg`}>Wicket Number</Text>
+                      <TouchableOpacity onPress={() => setShowWicketNumberPicker(true)}>
+                      <MaterialIcon name="add" size={24} />
+                      </TouchableOpacity>
+                    </View>
+              
+                    <View style={tailwind`flex flex-row justify-between mb-4`}>
+                      <Text style={tailwind`text-lg`}>Overs</Text>
+                      <TouchableOpacity onPress={() => setShowOverPicker(true)}>
+                      <MaterialIcon name="add" size={24} />
+                      </TouchableOpacity>
+                    </View>
+              
+                    <Pressable
+                      onPress={() => handleFallOfWickets()}
+                      style={tailwind`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md`}
+                    >
+                      <Text style={tailwind`text-lg`}>Update Bowling</Text>
+                    </Pressable>
+                  </View>
+                </View>
+
+                {showBatsmanPicker && (
+                  <Modal animationType="slide" transparent={true} visible={showBatsmanPicker}>
+                        <Pressable
+                            style={tailwind`flex-1 justify-center bg-black bg-opacity-50`}
+                            onPress={() => setShowBatsmanPicker(false)}
+                        >
+                            <View style={tailwind`bg-white rounded-md shadow-md p-4 m-40`}>
+                                <FlatList
+                                    data={battingData?.innings}
+                                    renderItem={({ item }) => (
+                                        <TouchableOpacity onPress={() => {
+                                            setCurrentBatsman(item.player.id);
+                                            setShowBatsmanPicker(false);
+                                        }}>
+                                            <Text style={tailwind`text-lg`}>{item.player.name}</Text>
+                                        </TouchableOpacity>
+                                    )}
+                                    keyExtractor={(item) => item.player.id.toString()}
+                                />
+                            </View>
+                        </Pressable>
+                  </Modal>
+                )}
+              
+              {showBowlerPicker && (
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={showBowlerPicker}
+                        onRequestClose={() => setShowBowlerPicker(false)}
+                    >
+                        <Pressable
+                            style={tailwind`flex-1 justify-center bg-black bg-opacity-50`}
+                            onPress={() => setShowBowlerPicker(false)}
+                        >
+                            <View style={tailwind`bg-white rounded-md shadow-md p-4 m-40`}>
+                                <FlatList
+                                    data={bowlingData?.innings}
+                                    renderItem={({ item }) => (
+                                        <TouchableOpacity
+                                            onPress={() => {
+                                                setCurrentBowler(item.player.id);
+                                                setShowBowlerPicker(false);
+                                            }}
+                                        >
+                                            <Text style={tailwind`text-lg`}>{item.player.name}</Text>
+                                        </TouchableOpacity>
+                                    )}
+                                    keyExtractor={(item) => item.player.id.toString()}
+                                />
+                            </View>
+                        </Pressable>
+                    </Modal>
+                )}
+
+              
+                {showWicketTypePicker && (
+                  <Modal animationType="slide" transparent={true} visible={showWicketTypePicker}>
+                    <Pressable
+                            style={tailwind`flex-1 justify-center bg-black bg-opacity-50`}
+                            onPress={() => setShowWicketTypePicker(false)}
+                        >
+                        <View style={tailwind`bg-white rounded-md shadow-md p-4 m-40`}>
+                            <FlatList
+                                data={wicketsTypes}
+                                renderItem={({ item }) => (
+                                    <TouchableOpacity onPress={() => {
+                                        setWicketType(item.value);
+                                        setShowWicketTypePicker(false);
+                                    }}>
+                                        <Text style={tailwind`text-lg`}>{item.label}</Text>
+                                    </TouchableOpacity>
+                                )}
+                                keyExtractor={(item) => item.key.toString()}
+                            />
+                        </View>
+                    </Pressable>
+                  </Modal>
+                )}
+              
+                {showWicketNumberPicker && (
+                  <Modal animationType="slide" transparent={true} visible={showWicketNumberPicker}>
+                    <Pressable
+                            style={tailwind`flex-1 justify-center bg-black bg-opacity-50`}
+                            onPress={() => setShowBowlerPicker(false)}
+                        >
+                        <View style={tailwind`bg-white rounded-md shadow-md p-4 m-40`}>
+                            <FlatList
+                            data={wicketsNumber}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity onPress={() => {
+                                    setWicketNumber(item.value);
+                                    setShowWicketNumberPicker(false);
+                                }}>
+                                    <Text style={tailwind`text-lg`}>{item.label}</Text>
+                                </TouchableOpacity>
+                            )}
+                            keyExtractor={(item) => item.key.toString()}
+                            />
+                    </View>
+                    </Pressable>
+                  </Modal>
+                )}
+                {showOversPicker && (
+                  <Modal animationType="slide" transparent={true} visible={showOversPicker} onRequestClose={() => setShowOverPicker(false)}>
+                    <Pressable
+                            style={tailwind`flex-1 justify-center bg-black bg-opacity-50`}
+                            onPress={() => setShowOverPicker(false)}
+                        >
+                        <View style={tailwind`bg-white rounded-md shadow-md p-4 m-40`}>
+                            {bowlingData.innings.map((item,index) => (
+
+                                    <TouchableOpacity key={index} onPress={() => {handleWicketsBall(item)}}>
+                                        {console.log("Itme: ", item)}
+                                        {console.log("Player: ", item.player.id)}
+                                        {console.log("current Bowler: ", currentBowler)}
+                                    {item.player.id === currentBowler  && (
+                                        <Text style={tailwind`text-lg`}>Balls Bowled: {item.ball}</Text>
+                                    )}
+                                    </TouchableOpacity>
+                            ))}
+                    </View>
+                    </Pressable>
+                  </Modal>
+                )}
+              </Modal>
             )}
         </View>
     );
