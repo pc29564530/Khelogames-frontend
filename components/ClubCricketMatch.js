57f4b0cc1c07aeed0c5acf85e5028cac539d41ba
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-08-26 13:55:24 +0530

    fix the team cricket match and remove the console

--- components/ClubCricketMatch.js
+++ components/ClubCricketMatch.js
@@ -1,20 +1,18 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useEffect, useState} from 'react';
-import { View, Text, Pressable, Image, Modal} from 'react-native';
+import { View, Text, Pressable, Image, Modal, ScrollView } from 'react-native';
 import tailwind from 'twrnc';
 import { BASE_URL } from '../constants/ApiConstants';
 import useAxiosInterceptor from '../screen/axios_config';
 import { useNavigation } from '@react-navigation/native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import {formattedDate, formattedTime} from '../utils/FormattedDateTime'
-import { ScrollView } from 'react-native-gesture-handler';
-import { determineMatchStatus } from '../utils/MatchStatus';
+import { formattedDate, formattedTime } from '../utils/FormattedDateTime';
 import { findTournamentByID, getTournamentBySport } from '../services/tournamentServices';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTournamentByIdAction, getTournamentBySportAction } from '../redux/actions/actions';
 
-const ClubCricketMatch = ({clubData}) => {
-    const [match, setMatch] = useState([]);
+const ClubCricketMatch = ({ teamData }) => {
+    const [matches, setMatches] = useState([]);
     const [isDropDownVisible, setIsDropDownVisible] = useState(false);
     const [tournamentName, setTournamentName] = useState([]);
     const axiosInstance = useAxiosInterceptor();
@@ -23,7 +21,7 @@ const ClubCricketMatch = ({clubData}) => {
     const dispatch = useDispatch();
     const tournaments = useSelector((state) => state.tournamentsReducers.tournaments);
     const tournament = useSelector((state) => state.tournamentsReducers.tournament);
-    const sport = useSelector(state => state.sportReducers.sport);
+    const sport = useSelector((state) => state.sportReducers.sport);
 
     useEffect(() => {
         fetchClubMatch();
@@ -32,156 +30,132 @@ const ClubCricketMatch = ({clubData}) => {
     const fetchClubMatch = async () => {
         try {
             const authToken = await AsyncStorage.getItem('AccessToken');
-            const response = await axiosInstance.get(`${BASE_URL}/${sport}/getMatchByClubName` ,{
+            const response = await axiosInstance.get(`${BASE_URL}/Cricket/getMatchByTeamFunc`, {
                 params: {
-                    id: clubData.id.toString()
+                    id: teamData.id.toString()
                 },
                 headers: {
                     'Authorization': `Bearer ${authToken}`,
                     'Content-Type': 'application/json',
                 },
             });
-            if(!response.data || response.data === null ){
-                setMatch([]);
-            } else {
-                const item = response.data;
-                const matchData = item.map( async (itm, index) => {
-                    try {
-                        const responseScore1 = await axiosInstance.get(`${BASE_URL}/${sport}/getCricketMatchScore`, {
-                            params:{
-                                match_id: itm.match_id,
-                                team_id: itm.team1_id
-                            },
-                            headers: {
-                                'Authorization': `Bearer ${authToken}`,
-                                'Content-Type': 'application/json',
-                            }
-                        })
-    
-                        const responseScore2 = await axiosInstance.get(`${BASE_URL}/${sport}/getCricketMatchScore`, {
-                            params:{
-                                match_id: itm.match_id,
-                                team_id: itm.team2_id
-                            },
-                            headers: {
-                                'Authorization': `Bearer ${authToken}`,
-                                'Content-Type': 'application/json',
-                            }
-                        })
-                        if(responseScore1.data === null && responseScore2.data === null) {
-                            return {...itm, responseScore1: [], responseScore2: []}
-                        } else {
-                            return {...itm, responseScore1: responseScore1.data, responseScore2: responseScore2.data}
-                        }
-                    } catch (err) {
-                        console.error("unable to get the score using club match", err)
-                    }   
-                })
 
-                const matchDataWithScore = await Promise.all(matchData);
-                setMatch(matchDataWithScore)
-                const tournamentIDSet = new Set();
-                const tournamentData = [];
-                item.forEach((itm) => {
-                    if(!tournamentIDSet.has(itm.tournament_id)){
-                        tournamentIDSet.add(itm.tournament_id)
-                        tournamentData.push({
-                            ...itm,
-                            tournament_id: itm?.tournament_id,
-                            tournament_name: itm?.tournament_name
-                        })
-                    }
-                });
-                setTournamentName(tournamentData);
-             }
+            if (!response.data) {
+                setMatches([]);
+            } else {
+                setMatches(response.data.filter(item => item !== null));
+            }
         } catch (err) {
-            console.log("unable to get the tournament match ", err);
+            console.log("unable to get the match by teams ", err);
         }
-    }
-    const handleFixtureStatus = async (item) => {
-        navigation.navigate("CricketMatchPage", {item:item})
-    }
+    };
+
+    const handleMatch = (item) => {
+        navigation.navigate("CricketMatchPage", { item });
+    };
 
     const handleDropDown = () => {
         setIsDropDownVisible(true);
-    }
+    };
 
     const handleTournamentNavigate = async (tournamentItem) => {
         const tournamentId = tournamentItem.tournament_id;
         const tournamentStatus = ["live", "previous", "upcoming"];
-        const tournamentBySport = await getTournamentBySport({axiosInstance, sport});
+        const tournamentBySport = await getTournamentBySport({ axiosInstance, sport });
         dispatch(getTournamentBySportAction(tournamentBySport));
-        const foundTournament = findTournamentByID({tournamentBySport, tournamentId, tournamentStatus});
-        if(foundTournament !== null){
+        const foundTournament = findTournamentByID({ tournamentBySport, tournamentId, tournamentStatus });
+        if (foundTournament !== null) {
             dispatch(getTournamentByIdAction(foundTournament));
         }
-        navigation.navigate("TournamentPage", {tournament: tournament, currentRole: currentRole});
-    }
+        navigation.navigate("TournamentPage", { tournament, currentRole });
+    };
+
+    let tournamentsID = new Set();
+    matches.map((item) => {
+        tournamentsID.add(item.tournament.id);
+    })
 
     return (
         <ScrollView style={tailwind`mt-4`}>
-            <Pressable style={tailwind`border rounded-lg flex-row items-center justify-center inline inline-block w-35 gap-2`} onPress={() => handleDropDown()}>
+            <Pressable style={tailwind`border rounded-lg flex-row items-center justify-center w-35 gap-2`} onPress={handleDropDown}>
                 <Text style={tailwind`text-lg text-black p-2`}>Tournament</Text>
-                <AntDesign name="down"  size={10} color="black" />
+                <AntDesign name="down" size={10} color="black" />
             </Pressable>
             <ScrollView>
-                {tournamentName.length>0 && match.map((item, index) => (
-                    <Pressable key={index} style={tailwind`mb-4 p-1 bg-white rounded-lg shadow-md`} onPress={() => handleFixtureStatus(item)} >
+                {matches.length > 0 && matches.map((item, index) => (
+                    <Pressable key={index} style={tailwind`mb-4 p-1 bg-white rounded-lg shadow-md`} onPress={() => handleMatch(item)} >
                         <View style={tailwind`items-start justify-center ml-2`}>
-                            <Text style={tailwind``}>{item?.tournament_name}</Text>
+                            <Text>{item?.tournament.name}</Text>
                         </View>
                         <View style={tailwind`flex-row items-center`}>
-                            <View style={tailwind` justify-between mb-2 gap-1 p-2 mt-1`}>
+                            <View style={tailwind`justify-between mb-2 gap-1 p-2 mt-1`}>
                                 <View style={tailwind`items-center flex-row`}>
-                                    <Image source={{ uri: item?.team1_avatar_url }} style={tailwind`w-6 h-6 bg-violet-200 rounded-full `} />
-                                    <Text style={tailwind`ml-2 text-md font-semibold text-gray-800`}>{item?.team1_name}</Text>
+                                    <View style={tailwind`flex-row`}>
+                                        <Image source={{ uri: item?.homeTeam.media_url }} style={tailwind`w-6 h-6 bg-violet-200 rounded-full`} />
+                                        <Text style={tailwind`ml-2 text-md font-semibold text-gray-800`}>{item?.homeTeam.name}</Text>
+                                    </View>
+                                    {item.status !== "not_started" && item.homeScore !== null && (
+                                        <View>
+                                            <Text style={tailwind`text-black`}>{item?.homeScore.score}/{item?.homeScore.wickets}</Text>
+                                        </View>
+                                    )}
                                 </View>
-                                <View style={tailwind` items-center flex-row`}>
-                                    <Image source={{ uri: item?.team2_avatar_url }} style={tailwind`w-6 h-6 bg-violet-200 rounded-full`} />
-                                    <Text style={tailwind`ml-2 text-md font-semibold text-gray-800`}>{item?.team2_name}</Text>
+                                <View style={tailwind`items-center flex-row`}>
+                                    <View style={tailwind`items-center flex-row`}>
+                                        <View style={tailwind`flex-row`}>
+                                            <Image source={{ uri: item?.homeTeam.media_url }} style={tailwind`w-6 h-6 bg-violet-200 rounded-full`} />
+                                            <Text style={tailwind`ml-2 text-md font-semibold text-gray-800`}>{item?.awayTeam.name}</Text>
+                                        </View>
+                                        {item.status !== "not_started"  && item.awayScore !== null && (
+                                            <View>
+                                                <Text style={tailwind`text-black`}>{item.awayScore.score}/{item.awayScore.wickets}</Text>
+                                            </View>
+                                        )}
+                                    </View>
                                 </View>
                             </View>
                             <View style={tailwind`bg-gray-400 w-0.2 mx-4 h-10`}></View>
-                            <View style={tailwind` justify-between`}>
-                            {(determineMatchStatus(item)==="Live" || determineMatchStatus(item) === "End")  &&  (
-                                <>
-                                    <Text style={tailwind`text-black`}>{item.responseScore1.score}/{item.responseScore1.wickets }</Text>
-                                    <Text style={tailwind`text-black`}>{item.responseScore2.score}/{item.responseScore2.wickets}</Text>
-                                </>
-                            )}
-                            {determineMatchStatus(item)==="Not Started" && (
-                                <>
-                                    <Text style={tailwind`text-black`}>{formattedDate(item.date_on)}</Text>
-                                    <Text style={tailwind`text-black`}>{formattedTime(item.start_time)}</Text>
-                                </>
-                            )}
+                            <View style={tailwind`justify-between`}>
+                                {item.status === "not started" ? (
+                                    <>
+                                        <Text style={tailwind`text-black`}>{formattedDate(item.date_on)}</Text>
+                                        <Text style={tailwind`text-black`}>{formattedTime(item.start_time)}</Text>
+                                    </>
+                                ):(
+                                   <Text>{item.status}</Text> 
+                                )}
                             </View>
                         </View>
-                </Pressable>
+                    </Pressable>
                 ))}
             </ScrollView>
-                {isDropDownVisible && (
-                    <Modal
-                        animationType="slide"
-                        transparent={true}
-                        visible={isDropDownVisible}
-                        onRequestClose = {() => setIsDropDownVisible(!isDropDownVisible)}
-                    >
-                        <Pressable onPress={() => setIsDropDownVisible(false)} style={tailwind`flex-1 justify-end bg-gray-900 bg-opacity-50 w-full`}>
-                            <View style={tailwind`bg-white rounded-md p-4`}>
-                                {tournamentName && tournamentName?.map((item, index) => (
+            {isDropDownVisible && (
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={isDropDownVisible}
+                    onRequestClose = {() => setIsDropDownVisible(!isDropDownVisible)}
+                >
+                    <Pressable style={tailwind`flex-1 justify-end bg-gray-900 bg-opacity-50 w-full`} onPress={() => setIsDropDownVisible(false)}>
+                        <View style={tailwind`bg-white rounded-md p-4`}>
+                            {[...tournamentsID]?.map((tournamentID, index) => {
+                                const tournamentItem = matches.find((item) => item.tournament.id === tournamentID );
+                                return (
                                     <Pressable
                                         key={index}
                                         style={tailwind`bg-white p-2`}
-                                        onPress={() => {handleTournamentNavigate(item)}}
+                                        onPress={() => {
+                                            handleTournamentNavigate(tournamentItem)
+                                        }}
                                     >
-                                        <Text>{item?.tournament_name}</Text>
+                                        <Text>{tournamentItem?.tournament?.name}</Text>
                                     </Pressable>
-                                ))}
-                            </View>
-                        </Pressable>
-                    </Modal>
-                )}
+                                )
+                                })}
+                        </View>
+                    </Pressable>
+                </Modal>
+            )}
         </ScrollView>
     );
 }
