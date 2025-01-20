2974fed59951108df50a58273e0e0cb1e869d017
Pawan <pawan@Pawans-MacBook-Pro.local>
2025-01-20 08:28:44 +0530

    order the batting and bowling player in scorecard and set the current bowler and batsman

--- components/CricketBattingScorecard.js
+++ components/CricketBattingScorecard.js
@@ -5,39 +5,39 @@ import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 const CricketBattingScorecard = ({ battingData, setIsModalBattingVisible, handleUpdatePlayerBatting }) => {
     return (
-        <View style={tailwind`bg-white rounded-lg shadow-md mb-4`}>
-            {/* Header */}
-            <View style={tailwind`flex-row justify-between px-6 py-2 bg-gray-200`}>
-                <Text style={tailwind`text-md font-bold text-gray-700`}>Batter</Text>
+        <View style={tailwind`bg-white mb-4 shadow-lg rounded-lg overflow-hidden`}>
+            <View style={tailwind`flex-row justify-between px-6 py-2`}>
+                <Text style={tailwind`text-md text-gray-700`}>Batter</Text>
                 <View style={tailwind`flex-row justify-between gap-4`}>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>R</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>B</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>4s</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>6s</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>Edit</Text>
+                    <Text style={tailwind`text-md text-gray-700`}>R</Text>
+                    <Text style={tailwind`text-md text-gray-700`}>B</Text>
+                    <Text style={tailwind`text-md text-gray-700`}>4s</Text>
+                    <Text style={tailwind`text-md text-gray-700`}>6s</Text>
                 </View>
             </View>
-
             {/* Batting Data */}
             {battingData?.innings?.map((item, index) => (
-                <View key={index} style={tailwind`flex-row justify-between mb-2 px-6 py-2 bg-white`}>
-                    <Text style={tailwind`text-md text-gray-800 flex-1`}>{item?.player?.name}</Text>
-                    <View style={tailwind`flex-row justify-between gap-4 flex-1`}>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.runsScored}</Text>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.ballFaced}</Text>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.fours}</Text>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.sixes}</Text>
-                        <Pressable onPress={() => handleUpdatePlayerBatting(item)}>
-                            <MaterialIcon name="update" size={24} color="blue" />
-                        </Pressable>
+                <View key={index} style={tailwind`flex-row justify-between mb-2 px-6 py-2 ${item.is_striker ? 'bg-red-100': 'bg-white'}`}>
+                    <View style={tailwind`flex-row`}>
+                        <Text style={tailwind`text-md text-gray-800`}>{item?.player?.name}</Text>
+                        {item.is_striker && <Text style={tailwind`text-md text-gray-800`}>*</Text>}
+                    </View>
+                    
+                    <View style={tailwind`flex-row justify-between gap-4`}>
+                        <Text style={tailwind`text-md text-gray-800`}>{item.runsScored}</Text>
+                        <Text style={tailwind`text-md text-gray-800`}>{item.ballFaced}</Text>
+                        <Text style={tailwind`text-md text-gray-800`}>{item.fours}</Text>
+                        <Text style={tailwind`text-md text-gray-800`}>{item.sixes}</Text>
                     </View>
                 </View>
             ))}
 
             {/* Add Next Batsman Button */}
-            <Pressable onPress={() => { setIsModalBattingVisible(true) }} style={tailwind`mt-4 p-2 bg-blue-500 rounded-lg shadow-md`}>
-                <Text style={tailwind`text-white text-center`}>Add Next Batsman</Text>
-            </Pressable>
+            <View style={tailwind`p-4`}>
+                <Pressable onPress={() => { setIsModalBattingVisible(true) }} style={tailwind`mt-4 p-3 bg-red-500 rounded-lg shadow-md`}>
+                    <Text style={tailwind`text-white text-center font-semibold`}>Add Next Batsman</Text>
+                </Pressable>
+            </View>
         </View>
     );
 };
--- components/CricketBowlingScorecard.js
+++ components/CricketBowlingScorecard.js
@@ -1,47 +1,61 @@
 import React from 'react';
 import { View, Text, Pressable } from 'react-native';
 import tailwind from 'twrnc';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
-const CricketBowlingScorecard = ({ bowlingData, setIsModalBowlingVisible, handleUpdatePlayerBowling, convertBallToOvers }) => {
-    return (
-        <View style={tailwind`bg-white rounded-lg shadow-md p-4 mb-4`}>
-            {/* Header */}
-            <View style={tailwind`flex-row justify-between px-6 py-2 bg-gray-200 rounded-lg`}>
-                <Text style={tailwind`text-md font-bold text-gray-700`}>Bowler</Text>
-                <View style={tailwind`flex-row justify-between gap-4`}>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>O</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>R</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>W</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>WD</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>NB</Text>
-                    <Text style={tailwind`text-md font-bold text-gray-700`}>Edit</Text>
-                </View>
-            </View>
-
-            {/* Bowling Data */}
-            {bowlingData?.innings?.map((item, index) => (
-                <View key={index} style={tailwind`flex-row justify-between px-6 py-2 bg-white border-b border-gray-200`}>
-                    <Text style={tailwind`text-md text-gray-800 flex-1`}>{item?.player?.name}</Text>
-                    <View style={tailwind`flex-row justify-between gap-4 flex-1`}>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{convertBallToOvers(item.ball)}</Text>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.runs}</Text>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.wickets}</Text>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.wide}</Text>
-                        <Text style={tailwind`text-md text-gray-800 text-center`}>{item.noBall}</Text>
-                        <Pressable onPress={() => handleUpdatePlayerBowling(item)}>
-                            <MaterialIcon name="update" size={24} color="blue" />
-                        </Pressable>
-                    </View>
-                </View>
-            ))}
-
-            {/* Add Next Bowler Button */}
-            <Pressable onPress={() => setIsModalBowlingVisible(true)} style={tailwind`mt-4 p-2 bg-blue-500 rounded-lg shadow-md`}>
-                <Text style={tailwind`text-white text-center`}>Add Next Bowler</Text>
-            </Pressable>
+const CricketBowlingScorecard = ({
+  bowlingData,
+  setIsModalBowlingVisible,
+  handleUpdatePlayerBowling,
+  convertBallToOvers,
+}) => {
+  return (
+    <View style={tailwind`bg-white shadow-lg rounded-lg overflow-hidden`}>
+      {/* Header */}
+      <View style={tailwind`flex-row justify-between px-4 py-2`}>
+        <Text style={tailwind`flex-1 text-md text-gray-700 font-semibold`}>Bowler</Text>
+        <View style={tailwind`flex-row flex-[3] justify-between`}>
+          <Text style={tailwind`w-8 text-md text-gray-700 font-semibold text-center`}>O</Text>
+          <Text style={tailwind`w-8 text-md text-gray-700 font-semibold text-center`}>R</Text>
+          <Text style={tailwind`w-8 text-md text-gray-700 font-semibold text-center`}>W</Text>
+          <Text style={tailwind`w-8 text-md text-gray-700 font-semibold text-center`}>WD</Text>
+          <Text style={tailwind`w-8 text-md text-gray-700 font-semibold text-center`}>NB</Text>
         </View>
-    );
+      </View>
+
+      {/* Bowling Data */}
+      {bowlingData?.innings?.map((item, index) => (
+        <View
+          key={index}
+          style={tailwind`flex-row justify-between px-4 py-2  ${item.is_current_bowler ? 'bg-red-100' : 'bg-white'}`}
+        >
+          {console.log("bowl: ", item)}
+            <View style={tailwind`flex-row`}>
+                <Text style={tailwind`text-md text-gray-700`}>{item?.player?.name}</Text>
+                {item.is_current_bowler && <Text style={tailwind`text-md text-gray-700`}>*</Text>}
+            </View>
+          <View style={tailwind`flex-row justify-between`}>
+            <Text style={tailwind`w-8 text-md text-gray-800 text-center`}>
+              {convertBallToOvers(item.ball)}
+            </Text>
+            <Text style={tailwind`w-8 text-md text-gray-800 text-center`}>{item.runs}</Text>
+            <Text style={tailwind`w-8 text-md text-gray-800 text-center`}>{item.wickets}</Text>
+            <Text style={tailwind`w-8 text-md text-gray-800 text-center`}>{item.wide}</Text>
+            <Text style={tailwind`w-8 text-md text-gray-800 text-center`}>{item.noBall}</Text>
+          </View>
+        </View>
+      ))}
+
+      {/* Add Next Bowler Button */}
+      <View style={tailwind`p-4`}>
+        <Pressable
+          onPress={() => setIsModalBowlingVisible(true)}
+          style={tailwind`mt-4 p-3 bg-blue-500 rounded-lg shadow-md`}
+        >
+          <Text style={tailwind`text-white text-center font-semibold`}>Add Next Bowler</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 };
 
 export default CricketBowlingScorecard;
