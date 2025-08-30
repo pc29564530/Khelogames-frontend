d748950072fbdd77615e29c53a252f3458ae1e17
Pawan <pc29564530@gmail.com>
2025-08-30 12:24:16 +0530

    refactor: update the ui implement flatlist in community member and fix the community message error

--- screen/CommunityMember.js
+++ screen/CommunityMember.js
@@ -19,14 +19,14 @@ function  CommunityMember({item, parentScrollY, headerHeight, collapsedHeader}) 
     });
 
     const handleProfile = (item) => {
-        navigation.navigate("Profile", {profilePublicID: item.profile.public_id})
+        navigation.navigate("Profile", {profilePublicID: item?.public_id})
     }
 
     const fetchCommunityMember = async () => {
         try {
             //change the url name getCommunityMember
             const authToken = await AsyncStorage.getItem('AccessToken');
-            const response = await axiosInstance.get(`${BASE_URL}/get_communities_member/${communityData.public_id}`,null, {
+            const response = await axiosInstance.get(`${BASE_URL}/getCommunityMember/${community.public_id}`,null, {
                 headers: {
                     'Authorization': `Bearer ${authToken}`,
                     'Content-Type': 'application/json',
@@ -45,24 +45,25 @@ function  CommunityMember({item, parentScrollY, headerHeight, collapsedHeader}) 
     }, []);
 
     const renderMemberItem = ({item}) => {
-            <View style={tailwind`h-10 flex-row gap-5 mt-5`}>
-                <Pressable style={tailwind`flex-row items-center p-2`} onPress={() => handleProfile(iten)}>
-                    {item.profile?.avatar_url ? (
-                        <Image source={{ uri: item.profile.avatar_url }} style={tailwind`w-12 h-12 aspect-w-1 aspect-h-1 rounded-full bg-red-500`} />
+        return (    
+            <View style={tailwind`flex-1 shadow-lg rounded-lg bg-white m-2`}>
+                <Pressable style={tailwind`flex-row items-center p-2`} onPress={() => handleProfile(item)}>
+                    {item?.avatar_url ? (
+                        <Image source={{ uri: item.avatar_url }} style={tailwind`w-12 h-12 aspect-w-1 aspect-h-1 rounded-full bg-red-500`} />
                     ) : (
                         <View style={tailwind`w-12 h-12 rounded-12 items-center bg-yellow-200 justify-center`}>
                         <Text style={tailwind`text-red-500 text-6x3`}>
-                            N/A
+                            {item.full_name && item.full_name.charAt(0).toUpperCase()}
                         </Text>
                         </View>
                     )}
                     <View style={tailwind`ml-3`}>
-                        <Text style={tailwind`font-bold text-black`}>{item.profile && item.profile.full_name ? item.profile.full_name : ''}</Text>
-                        <Text style={tailwind`text-black`}>@{item.profile.username}</Text>
+                        <Text style={tailwind`font-bold text-black`}>{ item.full_name}</Text>
+                        <Text style={tailwind`text-black`}>@{item.username}</Text>
                     </View>
                 </Pressable>
             </View>
-    }
+    )}
     return (
         <Animated.FlatList 
             data={communityWithProfile || []}
