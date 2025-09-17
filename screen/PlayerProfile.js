77bc8ddf81ad3cad5bde5169d3c63715c38c99f5
Pawan <pc29564530@gmail.com>
2025-09-17 17:44:13 +0530

    Added the top tab view and animated player profile

--- screen/PlayerProfile.js
+++ screen/PlayerProfile.js
@@ -1,192 +1,281 @@
-    import React, { useState, useEffect } from 'react';
-    import { Text, View, Image, TouchableOpacity, ActivityIndicator, Pressable, Alert } from 'react-native';
-    import tailwind from 'twrnc';
-    import { useNavigation } from '@react-navigation/native';
-    import axiosInstance from './axios_config';
-    import AsyncStorage from '@react-native-async-storage/async-storage';
-    import AntDesign from 'react-native-vector-icons/AntDesign';
-    import { BASE_URL } from '../constants/ApiConstants';
-    import { useSelector } from 'react-redux';
-    import { sportsServices } from '../services/sportsServices';
-    import { useDispatch } from 'react-redux';
-    import { setGames } from '../redux/actions/actions';
-    import { CricketPlayerStats } from '../components/PlayerStats';
-    import FootballPlayerStats from '../components/FootballPlayerStats';
+import React, { useState, useEffect } from 'react';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+  Pressable,
+  Alert,
+  ScrollView,
+} from 'react-native';
+import tailwind from 'twrnc';
+import { useNavigation } from '@react-navigation/native';
+import axiosInstance from './axios_config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { BASE_URL } from '../constants/ApiConstants';
+import { useSelector, useDispatch } from 'react-redux';
+import { sportsServices } from '../services/sportsServices';
+import { setGames } from '../redux/actions/actions';
+import { CricketPlayerStats } from '../components/PlayerStats';
+import FootballPlayerStats from '../components/FootballPlayerStats';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  Extrapolation,
+  interpolate,
+  interpolateColor,
+} from 'react-native-reanimated';
+import TopTabPlayer from '../navigation/TopTabPlayer';
 
-    const PlayerProfile = ({ route }) => {
-        const dispatch = useDispatch();
-        const {publicID, from} = route.params;
-        const [loading, setLoading] = useState(false);
-        const [player, setPlayer] = useState(null);
-        const navigation = useNavigation();
-        const games = useSelector(state => state.sportReducers.games);
-        const game = useSelector(state => state.sportReducers.game);
-        const [isOwner, setIsOwner] = useState(false);
-        const [playerSport, setPlayerSport] = useState(null);
-        const [headerContentType, setHeaderContentType] = useState("Matches");
+const PlayerProfile = ({ route }) => {
+  const dispatch = useDispatch();
+  const { publicID, from } = route.params;
+  const [loading, setLoading] = useState(false);
+  const [player, setPlayer] = useState(null);
+  const navigation = useNavigation();
+  const games = useSelector(state => state.sportReducers.games);
+  const game = useSelector(state => state.sportReducers.game);
+  const [isOwner, setIsOwner] = useState(false);
+  const authProfilePublicID = useSelector(state => state.profile.authProfilePublicID);
 
-        const user = useSelector(state => state.user.user);
-        const authProfilePublicID = useSelector(state => state.profile.authProfilePublicID)
-
-        useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const data = await sportsServices({ axiosInstance });
-                    dispatch(setGames(data));
-                } catch (error) {
-                    console.error("Unable to fetch games data: ", error);
-                }
-            };
-            fetchData();
-        }, []);
-
-        useEffect(() => {
-            const fetchPlayer = async () => {
-                try {
-                    setLoading(true);
-                    const authToken = await AsyncStorage.getItem("AccessToken");
-                    const userPublicID = await AsyncStorage.getItem("UserPublicID")
-                    if(from === "team" ){
-                        const playerResponse = await axiosInstance.get(`${BASE_URL}/getPlayer/${publicID}`, {
-                            headers:{
-                                'Authorization': `Bearer ${authToken}`,
-                                'Content-Type': 'application/json'
-                            }
-                        })
-                        if (playerResponse.data){
-                            setPlayer(playerResponse.data)
-                            setPlayerSport(games.find(gameItem => gameItem.id = playerResponse?.data.game_id))
-                        } else {
-                            setPlayer(null)
-                        }
-                    } else {
-                        const playerResponse = await axiosInstance.get(`${BASE_URL}/getPlayerByProfile/${publicID}`, {
-                            headers:{
-                                'Authorization': `Bearer ${authToken}`,
-                                'Content-Type': 'application/json'
-                            }
-                        })
-                        if (playerResponse.data){
-                            setPlayer(playerResponse.data)
-                            setPlayerSport(games.find(gameItem => gameItem.id = playerResponse?.data.game_id))
-                        } else {
-                            setPlayer(null)
-                        }
-                    }
-                } catch (err) {
-                    console.error("Failed to get player profile: ", err)
-                } finally {
-                    setLoading(false);
-                }
-            }
-            fetchPlayer()
-        }, []);
-
-
-        const handleAddActivity = () => {
-            if (isOwner){
-                navigation.navigate("CreatePlayerProfile");
-            } else {
-                Alert.alert("Not allowed to edit")
-            }
-        };
-
-        navigation.setOptions({
-            title: player?.name || "Player Profile",
-            headerLeft: () => {
-                return (
-                    <Pressable onPress={() => navigation.goBack()} style={tailwind`ml-2`}>
-                        <AntDesign name="arrowleft" size={24} color="white"/>
-                    </Pressable>
-                )
-            }
-        })
-
-        useEffect(() => {
-            const checkProfileOwner = async () => {
-                //check for currentOwner temp fix change with user_public_id
-                if (authProfilePublicID=== publicID) {
-                    setIsOwner(true);
-                }
-            };
-            checkProfileOwner();
-        }, []);
-
-        const handleContentType = (contentType) => {
-            if (contentType === "Matches") {
-                if(player.game_id === game.id && game.name === "football") {
-                    return (
-                        <Text style={tailwind`text-center text-gray-500 mt-10`}>
-                            Match history will appear here.
-                        </Text>
-                    )
-                } else if(player.game_id === game.id && game.name === "cricket") {
-                    return (
-                        <Text style={tailwind`text-center text-gray-500 mt-10`}>
-                            Match history will appear here.
-                        </Text>
-                    )
-                }
-            } else if(contentType === "Stats"){
-                if(player.game_id === game.id && game.name === "football") {
-                    return <FootballPlayerStats player={player} />;
-                } else if(player.game_id === game.id && game.name === "cricket") {
-                    return <CricketPlayerStats  player={player}/>;
-                }
-            }
+  const parentScrollY = useSharedValue(0);
+      const bgColor = '#ffffff';   // white
+      const bgColor2 = '#f87171'; //red-400
+      const headerHeight = 160;
+      const collapsedHeader = 50;
+      const offsetValue = headerHeight-collapsedHeader;
+      const headerStyle = useAnimatedStyle(() => {
+        const height = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [headerHeight, collapsedHeader],
+          Extrapolation.CLAMP,
+        )
+    
+        const backgroundColor = interpolateColor(
+          parentScrollY.value,
+          [0, offsetValue],
+          [bgColor2, bgColor2]
+        )
+        return {
+          backgroundColor, height
         }
+      })
 
-        const avatarStyle = tailwind`w-24 h-24 rounded-full bg-gray-200 items-center justify-center overflow-hidden`;
-        return (
-            <View style={tailwind`flex-1 bg-gray-50`}>
-                {loading ? (
-                    <View style={tailwind`flex-1 items-center justify-center`}>
-                        <ActivityIndicator size="large" color="#22c55e" />
-                    </View>
-                ) : player ? (
-                    <View style={tailwind`p-2`}>
-                        <View style={tailwind`flex-row items-center p-4 mb-6 bg-white`}>
-                            {player?.media_url ? (
-                                <Image style={avatarStyle} source={{ uri: player.media_url }} />
-                            ) : (
-                                <View style={avatarStyle}>
-                                    <Text style={tailwind`text-xl font-bold text-gray-700`}>
-                                        {player?.displayText || "N/A"}
-                                    </Text>
-                                </View>
-                            )}
-                            <View style={tailwind`ml-4`}>
-                                <Text style={tailwind`text-xl font-semibold text-gray-900`}>{player?.name}</Text>
-                                <Text style={tailwind`text-xl font-semibold text-gray-900`}>{player?.position}</Text>
-                                <Text style={tailwind`text-sm text-gray-500`}>{player?.country}</Text>
-                            </View>
-                        </View>
-                        <View style={tailwind`flex-row items-center p-4 mb-6 bg-white justify-evenly`}>
-                            {['Matches', 'Stats'].map((contentType, index) => (
-                                <Pressable key={index} onPress={() => setHeaderContentType(contentType)} style={tailwind`shadow-lg rounded-lg p-6`}>
-                                    <Text>{contentType}</Text>
-                                </Pressable>
-                            ))}
-                        </View>
-                        <View>
-                            {handleContentType(headerContentType)}
-                        </View>
-                    </View>
-                ) : (
-                    <View style={tailwind`mx-4 mt-16`}>
-                        <View style={tailwind`bg-white rounded-2xl p-10 shadow-lg items-center justify-center`}>
-                            <TouchableOpacity
-                                onPress={() => handleAddActivity()}
-                                activeOpacity={0.8}
-                                style={tailwind`bg-green-100 p-4 rounded-full mb-4`}>
-                                <AntDesign name="adduser" size={40} color="#22c55e" />
-                            </TouchableOpacity>
-                            <Text style={tailwind`text-lg font-semibold text-gray-800`}>Add Player Activity</Text>
-                        </View>
-                    </View>
-                )}
-            </View>
+      // Content container animation
+      const contentContainerStyle = useAnimatedStyle(() => {
+        const top = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [headerHeight, collapsedHeader],
+          Extrapolation.CLAMP,
         );
-    };
+      
+        return {
+          flex: 1,
+          marginTop: top,
+        };
+      });
 
-    export default PlayerProfile;
+      // Trophy animation
+      const avatarAnimatedStyle = useAnimatedStyle(() => {
+        const scale = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [1, 0.5], // big → small
+          Extrapolation.CLAMP,
+        );
+
+        const translateY = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [40, -20], // move up a bit
+          Extrapolation.CLAMP,
+        );
+
+        const translateX = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [0, -250],
+          Extrapolation.CLAMP,
+        );
+
+        return {
+          transform: [{ scale }, { translateX }, { translateY }],
+        };
+      });
+
+      // Text animation
+      const nameAnimatedStyles = useAnimatedStyle(() => {
+        const scale = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [1, 0.8],
+          Extrapolation.CLAMP,
+        );
+
+        const translateY = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [46, -96],
+          Extrapolation.CLAMP,
+        );
+
+        const translateX = interpolate(
+          parentScrollY.value,
+          [0, offsetValue],
+          [0, -80], 
+          Extrapolation.CLAMP,
+        );
+
+        return {
+          transform: [{ scale }, { translateX }, { translateY }],
+        };
+      });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await sportsServices({ axiosInstance });
+        dispatch(setGames(data));
+      } catch (error) {
+        console.error('Unable to fetch games data: ', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchPlayer = async () => {
+      try {
+        setLoading(true);
+        const authToken = await AsyncStorage.getItem('AccessToken');
+
+        let url =
+          from === 'team'
+            ? `${BASE_URL}/getPlayer/${publicID}`
+            : `${BASE_URL}/getPlayerByProfile/${publicID}`;
+
+        const playerResponse = await axiosInstance.get(url, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (playerResponse.data) {
+          setPlayer(playerResponse.data);
+        } else {
+          setPlayer(null);
+        }
+      } catch (err) {
+        console.error('Failed to get player profile: ', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPlayer();
+  }, []);
+
+  useEffect(() => {
+    if (authProfilePublicID === publicID) {
+      setIsOwner(true);
+    }
+  }, []);
+
+  const handleAddActivity = () => {
+    if (isOwner) {
+      navigation.navigate('CreatePlayerProfile');
+    } else {
+      Alert.alert('Not allowed to edit');
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={tailwind`flex-1 items-center justify-center`}>
+        <ActivityIndicator size="large" color="#ef4444" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={tailwind`flex-1 bg-gray-50`}>
+      {player ? (
+        <>
+          {/* Header */}
+          <Animated.View
+            style={[
+              headerStyle,
+              {
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                zIndex: 10,
+              },
+            ]}
+          >
+            <Pressable
+              onPress={() => navigation.goBack()}
+              style={tailwind`absolute left-2 top-4`}
+            >
+              <MaterialIcons name="arrow-back" size={22} color="white" />
+            </Pressable>
+            <View style={tailwind`items-center`}>
+            {player?.media_url ? (
+              <Animated.Image
+                style={[tailwind`h-20 w-20 rounded-full`, avatarAnimatedStyle]}
+                source={{ uri: player.media_url }}
+              />
+            ) : (
+              <Animated.View
+                style={[tailwind`h-20 w-20 rounded-full bg-white items-center justify-center shadow-lg`, avatarAnimatedStyle]}
+              >
+                <Text style={tailwind`text-3xl font-bold text-black`}>
+                  {player?.name?.charAt(0).toUpperCase()}
+                </Text>
+              </Animated.View>
+            )}
+                <Animated.View style={[tailwind`ml-4`, nameAnimatedStyles]}>
+                    <Text
+                        style={tailwind`text-xl font-bold text-white`}
+                        numberOfLines={1}
+                    >
+                        {player?.name}
+                    </Text>
+                    <Text style={tailwind`text-sm text-white`}>
+                        {player?.country}
+                    </Text>
+                </Animated.View>
+            </View>
+          </Animated.View>
+          <Animated.View style={[contentContainerStyle, tailwind`bg-white`]}>
+              <TopTabPlayer player={player} parentScrollY={parentScrollY} headerHeight={headerHeight} collapsedHeader={collapsedHeader}/>
+          </Animated.View>
+        </>
+      ) : (
+        <View style={tailwind`mx-4 mt-16`}>
+          <View
+            style={tailwind`bg-white rounded-2xl p-10 shadow-lg items-center justify-center`}
+          >
+            <TouchableOpacity
+              onPress={handleAddActivity}
+              activeOpacity={0.8}
+              style={tailwind`bg-red-100 p-4 rounded-full mb-4`}
+            >
+              <AntDesign name="adduser" size={40} color="#ef4444" />
+            </TouchableOpacity>
+            <Text style={tailwind`text-lg font-semibold text-gray-800`}>
+              Add Player Activity
+            </Text>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default PlayerProfile;
